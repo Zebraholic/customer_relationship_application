b71f3d5fa56be8559aca160bc5871a47e17a3eca
Zebraholic <rctavares@rogers.com>
2014-10-24 14:09:55 -0400

    Set up initial crm, contact and rolodex files.

---
+++ crm.rb
@@ -0,0 +1,31 @@
+class CRM
+
+	def print_main_menu
+	  puts "[1] Add a new contact"
+	  puts "[2] Modify an existing contact"
+	  puts "[3] Delete a contact"
+	  puts "[4] Display all the contacts"
+	  puts "[5] Display an attribute"
+	  puts "[6] Exit"
+	  puts "Enter a number: "
+	end
+
+	def main_menu
+	  print_main_menu
+	  user_selected = gets.to_i
+	  call_option(user_selected)
+	end
+
+	def call_option(user_selected)
+	  add_new_contact if user_selected == 1
+	  modify_existing_contact if user_selected == 2
+	  delete_a_contact if user_selected == 3
+	  
+	  # Finish off and do the rest for 3 through 6
+	  # To be clear, the methods add_new_contact and modify_existing_contact
+	  # haven't been defined yet
+	end
+
+end
+puts "\e[H\e[2J"
+
