d2e6800b8dc43360c14bf5d5a3d0cacf57247f2e
Zebraholic <rctavares@rogers.com>
2014-10-26 20:46:30 -0400

    Updated modify contact feature.

--- crm.rb
+++ crm.rb
@@ -8,8 +8,8 @@ class CRM
 	attr_reader :name 
 
   	def initialize(name)
-      @name = name 
-      @rolodex = Rolodex.new
+    	@name = name 
+    	@rolodex = Rolodex.new
   	end	
 
   	#Displays main menu from which selections are made [1] to [7].
@@ -45,7 +45,7 @@ class CRM
 	  when 1 then add_new_contact		
 	  when 2 then modify_existing_contact
 	  when 3 then display_one_contact
-	  when 4 then delete_a_contact
+	  when 4 then delete_contact
 	  when 5 then display_all_contacts
 	  when 6 then display_an_attribute
 	  when 7
@@ -73,34 +73,56 @@ class CRM
 	end
 
 	#[2] Method for modifying an existing contact.
-	def modify_existing_contact 	
-	  print "Change First Name: "
-	  first_name = gets.chomp
-	  print "Change Last Name: "
-	  last_name = gets.chomp
-	  print "Change Email Address: "
-	  email = gets.chomp
-	  print "Change your Note: "
-	  note = gets.chomp
-	  contact = Contact.new(first_name, last_name, email, note)
+	def modify_existing_contact 
+		  contact = display_one_contact	
+		  print "Change First Name: "
+		  first_name = gets.chomp
+		  print "Change Last Name: "
+		  last_name = gets.chomp
+		  print "Change Email Address: "
+		  email = gets.chomp
+		  print "Change your Note: "
+		  note = gets.chomp
+		  contact = Contact.new(first_name, last_name, email, note)
 	end
 
-	#[3] Mehtod for displaying a contact.
-	def display_one_contact(contact)	
-    	puts "Name: #{contact.first_name} #{contact.last_name}"
-    	puts "Email: #{contact.email}"
-    	puts "Notes: #{contact.note}"
-    	puts "Contact ID: #{contact.id}"
+	#[3] Method for displaying a contact.
+	def display_one_contact
+		puts "Enter Contact ID:"
+  		input = gets.chomp.to_i	
+  		@rolodex.contacts.each do |contact|
+  			if contact.id == input
+  				puts "Contact ID: #{contact.id}"
+    		puts "Name: #{contact.first_name} #{contact.last_name}"
+    		puts "Email: #{contact.email}"
+    		puts "Notes: #{contact.note}"
+    		else
+    			puts "Contact not found in system, please try again."
+    		end
+    	end
   	end
 
   	#[4] Method for deleting a contact.
+  	def delete_contact
+  		puts "Enter Contact ID to be deleted:"
+  		input = gets.chomp.to_i
+  		@rolodex.contacts.each do |contact|
+  			if contact.id == input
+  				@rolodex.contacts.delete(contact)
+  				puts "Contact Deleted."
+  			else
+  				puts "Contact not found in system, please try again"
+  				puts "or enter [5] to display list of all contacts."
+   			end
+   		end		
+  	end
 
 	#[5] Method for displaying a list of all contacts.
 	def display_all_contacts	
 	    @rolodex.contacts.each do |contact|
-	      puts "#{contact.id} #{contact.first_name} #{contact.last_name} <#{contact.email}>"
-	    end
-	    puts
+	    	puts "#{contact.id} #{contact.first_name} #{contact.last_name} <#{contact.email}>"
+			end
+		puts
 	end
 
 	#[6] Method for displaying a contact's selected attribute.
--- rolodex.rb
+++ rolodex.rb
@@ -7,10 +7,6 @@ class Rolodex
     @id = 10000
   end
 
-  def contacts
-    @contacts
-  end
-
   def add_contact(contact)
     contact.id = @id
     @contacts << contact
